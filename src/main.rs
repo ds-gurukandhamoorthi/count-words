496431a24cd70df1a836479960fa8d63c287e5d1
SWAMINATHAN Gurukandhamoorthi <ds.gurukandhamoorthi@gmail.com>
2020-06-18 11:03:04 +0200

    Count words from multiple files

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,32 @@
 use std::env;
 use std::fs::File;
-use std::io::{Write, BufReader, BufRead, Error};
+use std::io::{BufReader, BufRead, Error};
 use std::collections::HashMap;
 
 fn main() -> Result<(), Error> {
-    let filename  = env::args().skip(1).next().unwrap();
+    let files  = env::args().skip(1);
     let mut words_count = HashMap::new();
 
-    let input = File::open(filename)?;
+    for filename in files {
 
-    let buffered = BufReader::new(input);
+        let input = File::open(filename)?;
 
-    for line in buffered.lines() {
-        let line = line?;
-        for word in line.split(|c| char::is_whitespace(c) || char::is_ascii_punctuation(&c) || char::is_ascii_digit(&c)){
-            *words_count.entry(String::from(word)).or_insert(0_u32) += 1;
+        let buffered = BufReader::new(input);
+
+        for line in buffered.lines() {
+            let line = line?;
+            for word in line.split(|c| char::is_whitespace(c) || char::is_ascii_punctuation(&c) || char::is_ascii_digit(&c)){
+                *words_count.entry(String::from(word)).or_insert(0_u32) += 1;
+            }
         }
-    }
 
+    }
     println!("{},{}", "word", "count");
     for (word, count) in words_count {
-        println!("{},{}", word, count);
+        if !word.is_empty() {
+            println!("{},{}", word, count);
+        }
     }
-
-    // println!("{:?}", words_count);
 
     Ok(())
 }
